793cf51d527a094ad4903b5c5c8b7330b0db7f9c
robMolloy <robmolloy@hotmail.co.uk>
2024-07-12 00:39:55 +0100

    sort layout show auth form

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -17,9 +17,9 @@ export default function App({ Component, pageProps }: AppProps) {
   }, []);
   return (
     <Layout>
-      {authStore.isLoggedIn ? (
-        <Component {...pageProps} />
-      ) : (
+      {authStore.isLoggedIn === undefined && <div>Loading...</div>}
+      {authStore.isLoggedIn === true && <Component {...pageProps} />}
+      {authStore.isLoggedIn === false && (
         <Typography fullPage>
           <UserAuthCreateLoginForm />
         </Typography>
