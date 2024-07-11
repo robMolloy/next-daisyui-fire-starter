dafdbc471c1076f03d763023338a8de5d30ef65e
robMolloy <robmolloy@hotmail.co.uk>
2024-07-12 00:18:49 +0100

    add firebase storage and emulators to project

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -1,17 +1,25 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
-import { getAuth } from "firebase/auth";
+import { connectAuthEmulator, getAuth } from "firebase/auth";
+import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
 
-if (Math.random() > 0)
-  throw new Error(
-    "you will need to add firebase config for this to work: src/config/firebaseConfig.ts"
-  );
+const firebaseConfig = {
+  apiKey: "AIzaSyBSTs7Lj0jmNn6Y0wrz-KSKVWN6DXusy-Q",
+  authDomain: "next-firebase-emulator.firebaseapp.com",
+  projectId: "next-firebase-emulator",
+  storageBucket: "next-firebase-emulator.appspot.com",
+  messagingSenderId: "1064012878807",
+  appId: "1:1064012878807:web:76cdd163fc5277b2ceb192",
+  measurementId: "G-V4DV7EWKPV",
+};
 
-export const app = initializeApp({
-  /* YOUR CONFIG GOES HERE */
-});
-export const db = getFirestore(app);
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth();
-export const storage = getStorage(app);
+export const db = getFirestore(app);
+
+if (process.env.NEXT_PUBLIC_USE_FIREBASE_EMULATOR === "true") {
+  connectFirestoreEmulator(db, "127.0.0.1", 8080);
+  connectAuthEmulator(auth, "http://127.0.0.1:9099");
+}
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,15 +1,41 @@
 import { Typography } from "@/components";
+import { auth, db } from "@/config/firebaseConfig";
+import { addDoc, collection, onSnapshot } from "firebase/firestore";
+import { useEffect, useState } from "react";
 
-const delay = async (x: number) => {
-  return new Promise((resolve) => {
-    setTimeout(() => resolve(true), x);
-  });
-};
+const collName = "diceRolls";
 
 const Parent = () => {
+  const [docs, setDocs] = useState<any[]>([]);
+
+  useEffect(() => {
+    const colRef = collection(db, collName);
+    const unsubscribe = onSnapshot(colRef, (snapshot) => {
+      const documents = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setDocs(documents);
+    });
+
+    return () => unsubscribe();
+  }, []);
   return (
     <main className={`min-h-screen`}>
-      <Typography fullPage>Welcome</Typography>
+      <Typography fullPage>
+        <button
+          className="btn"
+          onClick={() => {
+            addDoc(collection(db, collName), {
+              uid: auth.currentUser?.uid,
+              number: Math.floor(Math.random() * 10000000),
+            });
+          }}
+        >
+          add something
+        </button>
+        {<pre>{JSON.stringify(docs, undefined, 2)}</pre>}
+      </Typography>
     </main>
   );
 };
