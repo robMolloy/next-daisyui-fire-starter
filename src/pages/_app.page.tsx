76f114316e8826e65253a635de6fbd29a087c0a2
robMolloy <robmolloy@hotmail.co.uk>
2024-07-19 22:59:50 +0100

    useAuthStore safeStore

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -2,24 +2,27 @@ import { Typography } from "@/components";
 import { auth } from "@/config/firebaseConfig";
 import { UserAuthCreateLoginForm } from "@/modules/authUserForm";
 import { Layout } from "@/modules/layout";
-import { useAuthStore } from "@/stores/useAuthStore";
+import { useAuthStore, useAuthStoreBase } from "@/stores/useAuthStore";
 import "@/styles/globals.css";
 import { onAuthStateChanged } from "firebase/auth";
 import type { AppProps } from "next/app";
 import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const authStoreBase = useAuthStoreBase();
   const authStore = useAuthStore();
+  const safeAuthStore = authStore.getSafeStore();
+
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
-      authStore.setIsLoggedIn(!!user);
+      authStoreBase.setUser(user);
     });
   }, []);
   return (
     <Layout>
-      {authStore.isLoggedIn === undefined && <div>Loading...</div>}
-      {authStore.isLoggedIn === true && <Component {...pageProps} />}
-      {authStore.isLoggedIn === false && (
+      {safeAuthStore.status === "loading" && <div>Loading...</div>}
+      {safeAuthStore.status === "logged_in" && <Component {...pageProps} />}
+      {safeAuthStore.status === "logged_out" && (
         <Typography fullPage>
           <UserAuthCreateLoginForm />
         </Typography>
--- src/stores/useAuthStore.ts
+++ src/stores/useAuthStore.ts
@@ -1,10 +1,27 @@
+import { User } from "firebase/auth";
 import { create } from "zustand";
 
 type TAuthState = {
-  isLoggedIn: undefined | boolean;
-  setIsLoggedIn: (x: undefined | boolean) => void;
+  user: undefined | null | User;
+  setUser: (x: null | User) => void;
 };
-export const useAuthStore = create<TAuthState>()((set, _) => ({
-  isLoggedIn: undefined,
-  setIsLoggedIn: (isLoggedIn) => set(() => ({ isLoggedIn })),
+export const useAuthStoreBase = create<TAuthState>()((set, _) => ({
+  user: undefined,
+  setUser: (user) => set(() => ({ user })),
 }));
+
+export const useAuthStore = () => {
+  const authStoreBase = useAuthStoreBase();
+  const user = authStoreBase.user;
+  const isLoggedIn = !!authStoreBase.user ? true : authStoreBase.user;
+
+  return {
+    getSafeStore: () => {
+      if (user === undefined) return { status: "loading" } as const;
+      if (user === null) return { status: "logged_out" } as const;
+      return { status: "logged_in", user } as const;
+    },
+    user,
+    isLoggedIn,
+  };
+};
