12b5742832be43bd49bb773c46b5f86839ac7a06
robMolloy <robmolloy@hotmail.co.uk>
2024-07-26 15:32:09 +0100

    input flexibility and remove border

--- src/components/inputs/Input.tsx
+++ src/components/inputs/Input.tsx
@@ -1,24 +1,32 @@
+const sizeLabelGapMap = {
+  "input-sm": "mb-0",
+  "input-md": "mb-1",
+};
+
 export const TextInput = (p: {
   type?: "text" | "password";
-  showError: boolean;
+  size?: "input-sm" | "input-md";
+  showError?: boolean;
   errorMessage?: string;
   value: string;
   label: string;
   placeholder: string;
   onInput: (x: string) => void;
 }) => {
-  const { type = "text" } = p;
+  const { type = "text", size = "input-md" } = p;
   return (
     <label className="form-control w-full">
-      <div className="label mb-2 p-0">
-        <span className={`label-text rounded py-1 ${p.showError ? "w-full bg-error" : ""}`}>
+      <div className={`label ${sizeLabelGapMap[size]} p-0`}>
+        <span
+          className={`label-text rounded py-1 ${p.showError ? "w-full bg-error text-center" : ""}`}
+        >
           {p.showError ? p.errorMessage : p.label}
         </span>
       </div>
       <input
         type={type}
         placeholder={p.placeholder}
-        className={`input input-bordered w-full ${p.showError ? "input-error" : "input-info"}`}
+        className={`input input-bordered ${size} w-full ${p.showError ? "input-error" : ""}`}
         onInput={(e) => {
           const value = (e.target as HTMLInputElement).value;
           p.onInput(value);
