258363ab7dfd7a00b71e3f33cf8335afabe81e69
robMolloy <robmolloy@hotmail.co.uk>
2024-07-26 15:36:06 +0100

    add heroicons and use in navbar

--- src/modules/layout/Layout.tsx
+++ src/modules/layout/Layout.tsx
@@ -4,6 +4,7 @@ import { useAuthStore } from "@/stores/useAuthStore";
 import { logoutFirebaseUser } from "@/utils/firebaseAuthUtils";
 import { auth } from "@/config/firebaseConfig";
 import { ThemeSelector } from "../themeSelector";
+import { Bars3Icon } from "@heroicons/react/24/outline";
 
 export type TPageLink = {
   label: string;
@@ -44,22 +45,6 @@ const ContainerWithSpotlightBackgroundTop = () => {
   );
 };
 
-const BurgerMenuIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    className="inline-block h-6 w-6 stroke-current"
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth="2"
-      d="M4 6h16M4 12h16M4 18h16"
-    ></path>
-  </svg>
-);
-
 export const Layout = (p: { children: React.ReactNode }) => {
   const authStore = useAuthStore();
   const safeAuthStore = authStore.getSafeStore();
@@ -71,10 +56,10 @@ export const Layout = (p: { children: React.ReactNode }) => {
           <NavBarContainer>
             <NavBar
               leftChildren={
-                <div className="flex gap-2 pl-4">
+                <div className="flex items-center gap-2 pl-4">
                   <div className="block sm:hidden">
                     <OpenDrawerWrapper>
-                      <BurgerMenuIcon />
+                      <Bars3Icon />
                     </OpenDrawerWrapper>
                   </div>
                   <Link href="/" className="p-0 text-3xl hover:underline">
