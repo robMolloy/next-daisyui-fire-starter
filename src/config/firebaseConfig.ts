a5044cce6465fa25578164b8f1e5c15406417b1a
robMolloy <robmolloy@hotmail.co.uk>
2024-08-14 14:24:15 +0100

    firebase seeds and config

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -3,10 +3,15 @@ import { initializeApp } from "firebase/app";
 import { connectAuthEmulator, getAuth } from "firebase/auth";
 import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
 
+export const useEmulator = process.env.NEXT_PUBLIC_USE_FIREBASE_EMULATOR === "true";
+export const emulatorOffline = process.env.NEXT_PUBLIC_USE_FIREBASE_EMULATOR_OFFLINE === "true";
+
+const projectId = "next-firebase-emulator";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBSTs7Lj0jmNn6Y0wrz-KSKVWN6DXusy-Q",
   authDomain: "next-firebase-emulator.firebaseapp.com",
-  projectId: "next-firebase-emulator",
+  projectId: `${emulatorOffline ? "demo-" : ""}${projectId}`,
   storageBucket: "next-firebase-emulator.appspot.com",
   messagingSenderId: "1064012878807",
   appId: "1:1064012878807:web:76cdd163fc5277b2ceb192",
@@ -19,7 +24,7 @@ export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
 export const db = getFirestore(app);
 
-if (process.env.NEXT_PUBLIC_USE_FIREBASE_EMULATOR === "true") {
+if (useEmulator) {
   connectFirestoreEmulator(db, "127.0.0.1", 8080);
   connectAuthEmulator(auth, "http://127.0.0.1:9099");
 }
