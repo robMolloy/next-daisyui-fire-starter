60104e16d16f20bc5dbecc46456afc3d5a38125a
robMolloy <robmolloy@hotmail.co.uk>
2024-08-14 15:54:41 +0100

    improved firebase config

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -6,17 +6,17 @@ import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
 export const useEmulator = process.env.NEXT_PUBLIC_USE_FIREBASE_EMULATOR === "true";
 export const emulatorOffline = process.env.NEXT_PUBLIC_USE_FIREBASE_EMULATOR_OFFLINE === "true";
 
-const projectId = "next-firebase-emulator";
-
-const firebaseConfig = {
+const initFirebaseConfig = {
   apiKey: "AIzaSyBSTs7Lj0jmNn6Y0wrz-KSKVWN6DXusy-Q",
   authDomain: "next-firebase-emulator.firebaseapp.com",
-  projectId: `${emulatorOffline ? "demo-" : ""}${projectId}`,
+  projectId: "next-firebase-emulator",
   storageBucket: "next-firebase-emulator.appspot.com",
   messagingSenderId: "1064012878807",
   appId: "1:1064012878807:web:76cdd163fc5277b2ceb192",
   measurementId: "G-V4DV7EWKPV",
 };
+const projectId = `${useEmulator ? "demo-" : ""}${initFirebaseConfig.projectId}`;
+const firebaseConfig = { ...initFirebaseConfig, projectId };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
